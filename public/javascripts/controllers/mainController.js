eccc55b58105ff8ef9cc6a801d941b516f41c52b
Itay242 <Itay242@gmail.com>
2014-12-29 00:43:16 +0200

    Tzvika is gay

--- public/javascripts/controllers/mainController.js
+++ public/javascripts/controllers/mainController.js
@@ -1,6 +1,7 @@
 app.controller('mainCtrl', ['$scope', 'usersFactory', function($scope, usersFactory){
     usersFactory.getUsers().then(function(response){
         $scope.users = response.data;
+        $scope.tzvika = "gay";
     });
 
     $scope.test = "hello";
