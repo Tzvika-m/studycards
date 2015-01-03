2c9376052fa997804f84fbc51809d2571b50f87c
Tzvika-m <tzvika919@gmail.com>
2015-01-03 23:44:01 +0200

    added schools factory&route

--- models/school.js
+++ models/school.js
@@ -6,6 +6,6 @@ var mongoose = require('mongoose');
 var schoolSchema = new mongoose.Schema({
     name : String,
     departments : [{type : mongoose.Schema.Types.ObjectId, ref: 'department'}]
-});
+}, { collection : "schools"});
 
 mongoose.model('school', schoolSchema);
